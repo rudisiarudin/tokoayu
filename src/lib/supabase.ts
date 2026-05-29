6f09a255603c8ca500a0b075d618b98c3de4a307
rudisiarudin <rudi.siarudin@gmail.com>
2026-05-29 22:17:19 +0700

    refactor: use environment variables for Supabase connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qbqsjcttipjalgigqqmx.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFicXNqY3R0aXBqYWxnaWdxcW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTcyNjYsImV4cCI6MjA5NTYzMzI2Nn0.w4LIUNvJYzTl7ayBw8fNB_SWTpYplg6vxJkEFjWBUeE";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
